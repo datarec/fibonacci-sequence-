570d96f179316ac7d86a5af27aff178d9fbe0b71
coolpancakes <73265375+coolpancakes@users.noreply.github.com>
2024-06-23 13:56:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-# fibonacci sequence in python 
+# Fibonacci sequence in python. 23/06/2024 
 
-count = 0
-values = [0, 1]
-while count != 25: 
-    seq = values[0] + values[1]
-    print(seq)
-    values.append(seq) 
-    values.pop(0)
-    count += 1
+def main():
+    # Variable that holds the number of loops the program is up to
+    count = 0
+
+    # List used to store expression of seq
+    values = [0, 1]
+    
+    while count != 25: 
+        seq = values[0] + values[1]
+
+        # Prints the seq expression
+        print(seq)
+
+        # Appends the expression 
+        values.append(seq) 
+
+        # Removes first index 
+        values.pop(0)
+
+        # Adds 1 to count variables to keep count of loops. 
+        count += 1
+
+
+if __name__ == "__main__":
+    
